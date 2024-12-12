f6e1ec2d5a2ed91a4669b38a183af4885a5128db
Fahad <fahad.45854@iqra.edu.com>
2024-12-13 01:05:21 +0500

    UpperBanner defind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Navbar from '@/components/Navbar'
+import UpperBanner from '@/components/UpperBanner'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
+      <UpperBanner />
       <Navbar />
     </div>
   )
