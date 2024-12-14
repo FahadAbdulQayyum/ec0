dc01e1f613d9a290177bd0b9bdc0729f0021fec5
Fahad Abdul Qayyum <fahad.45843@iqra.edu.pk>
2024-12-14 17:59:09 +0500

    Navbar bar removed in small screen but appears on large screen

--- src/components/UpperBanner/index.tsx
+++ src/components/UpperBanner/index.tsx
@@ -20,13 +20,13 @@ const UpperBanner = () => {
             {/* Right Portion */}
             <div className="right-portion w-full md:w-auto">
                 <ul className="flex flex-col md:flex-row justify-center md:justify-end space-y-2 md:space-y-0 text-sm">
-                    <li className="border-black md:pr-4 relative after:content-[''] after:absolute after:top-0 after:right-0 after:h-full after:w-[1px] after:bg-black md:last:after:hidden hover:underline cursor-pointer">
+                    <li className="border-black pr-4 relative after:content-[''] md:after:absolute after:top-0 after:right-0 after:h-full after:w-[1px] after:bg-black md:last:after:hidden hover:underline cursor-pointer">
                         Find a Store
                     </li>
-                    <li className="border-black md:pr-4 relative after:content-[''] after:absolute after:top-0 after:right-0 after:h-full after:w-[1px] after:bg-black md:last:after:hidden hover:underline cursor-pointer ml-4">
+                    <li className="border-black md:pr-4 relative after:content-[''] md:after:absolute after:top-0 after:right-0 after:h-full after:w-[1px] after:bg-black md:last:after:hidden hover:underline cursor-pointer ml-4">
                         Help
                     </li>
-                    <li className="border-black md:pr-4 relative after:content-[''] after:absolute after:top-0 after:right-0 after:h-full after:w-[1px] after:bg-black md:last:after:hidden ml-4">
+                    <li className="border-black md:pr-4 relative after:content-[''] md:after:absolute after:top-0 after:right-0 after:h-full after:w-[1px] after:bg-black md:last:after:hidden ml-4">
                         <Link href="/Sign/up" aria-label="Join Nike Membership" className="hover:underline">
                             Join Us
                         </Link>
